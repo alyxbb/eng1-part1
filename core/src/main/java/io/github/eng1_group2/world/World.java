7d04054e36eee69eb6bba5016a5bbec8e9a8a9d7
alyxbb <40714465+alyxbb@users.noreply.github.com>
2024-10-21 00:12:59 +0100

    feat: big buildings (#12)
    * feat: big buildings
    
    * chore: formatting

--- core/src/main/java/io/github/eng1_group2/world/World.java
+++ core/src/main/java/io/github/eng1_group2/world/World.java
@@ -4,22 +4,24 @@ package io.github.eng1_group2.world;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import io.github.eng1_group2.utils.Vec2;
 import io.github.eng1_group2.world.building.Building;
 import io.github.eng1_group2.world.building.BuildingType;
-import io.github.eng1_group2.utils.Vec2;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class World {
     private final Vec2 gridSize = new Vec2(10, 10);
-    private final HashMap<Vec2, Building> buildings;
+    private final List<Building> buildings;
+
 
     public World() {
-        this.buildings = new HashMap<>();
-        this.buildings.put(new Vec2(2, 3), new Building(new BuildingType("house", Color.RED)));
-        this.buildings.put(new Vec2(0, 1), new Building(new BuildingType("lecture_theatre", Color.BLUE)));
-        this.buildings.put(new Vec2(0, 2), new Building(new BuildingType("cafe", Color.GREEN)));
+        this.buildings = new ArrayList<>();
+        this.buildings.add(new Building(new BuildingType("house", Color.RED, new Vec2(2, 2)), new Vec2(3, 4)));
+        this.buildings.add(new Building(new BuildingType("lecture_theatre", Color.BLUE, new Vec2(1, 1)), new Vec2(0, 1)));
+        this.buildings.add(new Building(new BuildingType("cafe", Color.GREEN, new Vec2(3, 1)), new Vec2(0, 2)));
     }
 
     public void renderBuildings(Camera camera) {
@@ -40,11 +42,9 @@ public class World {
         shapeRenderer.end();
 
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        for (var entry : this.buildings.entrySet()) {
-            Vec2 coords = entry.getKey();
-            Building building = entry.getValue();
+        for (var building : this.buildings) {
             shapeRenderer.setColor(building.getType().color());
-            shapeRenderer.rect(coords.x() * gridUnit, coords.y() * gridUnit, gridUnit, gridUnit);
+            shapeRenderer.rect(building.getOrigin().x() * gridUnit, building.getOrigin().y() * gridUnit, gridUnit * building.getType().size().x(), gridUnit * building.getType().size().y());
         }
         shapeRenderer.end();
     }
--- core/src/main/java/io/github/eng1_group2/world/building/Building.java
+++ core/src/main/java/io/github/eng1_group2/world/building/Building.java
@@ -1,10 +1,18 @@
 package io.github.eng1_group2.world.building;
 
+import io.github.eng1_group2.utils.Vec2;
+
 public class Building {
     private final BuildingType type;
+    private final Vec2 origin;
 
-    public Building(BuildingType type) {
+    public Building(BuildingType type, Vec2 origin) {
         this.type = type;
+        this.origin = origin;
+    }
+
+    public Vec2 getOrigin() {
+        return origin;
     }
 
     public BuildingType getType() {
