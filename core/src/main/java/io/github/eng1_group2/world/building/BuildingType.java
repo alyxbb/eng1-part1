fb7d698df6abaa3c57083e7de40237fbb4d583ee
Ash <ash@ashhhleyyy.dev>
2024-10-20 21:40:38 +0100

    chore: refactor Vec2 and BuildingType to be records rather than classes (#6)

--- core/src/main/java/io/github/eng1_group2/world/building/BuildingType.java
+++ core/src/main/java/io/github/eng1_group2/world/building/BuildingType.java
@@ -2,20 +2,5 @@ package io.github.eng1_group2.world.building;
 
 import com.badlogic.gdx.graphics.Color;
 
-public class BuildingType {
-    private final String name;
-    private final Color color;
-
-    public BuildingType(String name, Color color) {
-        this.name = name;
-        this.color = color;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Color getColor() {
-        return color;
-    }
+public record BuildingType(String name, Color color) {
 }
