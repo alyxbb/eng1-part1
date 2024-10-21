f8da4a3caad3991b9796ef409960176f81678a2f
Ashhhleyyy <ash@ashhhleyyy.dev>
2024-10-22 00:31:26 +0100

    feat: move building types into a registry, in preparation for loading from assets

---
+++ core/src/main/java/io/github/eng1_group2/registry/Registry.java
@@ -0,0 +1,47 @@
+package io.github.eng1_group2.registry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A registry is an id->object map for a specific type of object.
+ * <p>
+ * Registries can be frozen by calling {@link Registry#freeze()}, which prevents modification once they have been initialised.
+ *
+ * @param <T> The type of object stored in this registry. Must implement {@link RegistryObject}
+ */
+public class Registry<T extends RegistryObject> {
+    private final String name;
+    private final Map<String, T> contents;
+    private boolean frozen = false;
+
+    public Registry(String name) {
+        this.name = name;
+        this.contents = new HashMap<>();
+    }
+
+    public T get(String id) {
+        if (!this.contents.containsKey(id)) {
+            throw new IllegalArgumentException("`" + id + "` not in registry `" + this.name + "`");
+        }
+        return this.contents.get(id);
+    }
+
+    public void register(T object) {
+        if (this.frozen) {
+            throw new IllegalStateException("`" + this.name + "` is frozen");
+        }
+        String id = object.getId();
+        if (this.contents.containsKey(id)) {
+            throw new IllegalArgumentException("`" + id + "` is already registered in `" + this.name + "`");
+        }
+        this.contents.put(id, object);
+    }
+
+    public void freeze() {
+        if (this.frozen) {
+            throw new IllegalStateException("`" + this.name + "` is frozen");
+        }
+        this.frozen = true;
+    }
+}
--- core/src/main/java/io/github/eng1_group2/world/building/BuildingType.java
+++ core/src/main/java/io/github/eng1_group2/world/building/BuildingType.java
@@ -1,7 +1,12 @@
 package io.github.eng1_group2.world.building;
 
 import com.badlogic.gdx.graphics.Color;
+import io.github.eng1_group2.registry.RegistryObject;
 import io.github.eng1_group2.utils.Vec2;
 
-public record BuildingType(String name, Color color, Vec2 size) {
+public record BuildingType(String id, String name, Color color, Vec2 size) implements RegistryObject {
+    @Override
+    public String getId() {
+        return this.id;
+    }
 }
