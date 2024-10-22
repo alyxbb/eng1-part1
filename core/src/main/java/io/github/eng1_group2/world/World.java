be18a16921b1d63a8b55624485e941fdbff46496
Alyx BB <git@alyxbb.tech>
2024-10-22 17:10:05 +0100

    input multiplexer

--- core/src/main/java/io/github/eng1_group2/world/World.java
+++ core/src/main/java/io/github/eng1_group2/world/World.java
@@ -2,7 +2,6 @@ package io.github.eng1_group2.world;
 
 
 import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.Viewport;
